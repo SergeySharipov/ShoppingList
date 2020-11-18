bd7bd225b808daf10410382aac4e34504fac5b84
SergeySharipov <sharipov14@gmail.com>
2020-11-17 21:23:04 -0500

    Create view model for ShoppingActivity.kt

---
+++ app/src/main/java/ca/sharipov/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -0,0 +1,21 @@
+package ca.sharipov.shoppinglist.ui.shoppinglist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import ca.sharipov.shoppinglist.R
+import ca.sharipov.shoppinglist.data.db.ShoppingDatabase
+import ca.sharipov.shoppinglist.data.repositories.ShoppingRepository
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
+
+        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/ca/sharipov/shoppinglist/ui/shoppinglist/ShoppingViewModel.kt
@@ -0,0 +1,23 @@
+package ca.sharipov.shoppinglist.ui.shoppinglist
+
+import androidx.lifecycle.ViewModel
+import ca.sharipov.shoppinglist.data.db.entities.ShoppingItem
+import ca.sharipov.shoppinglist.data.repositories.ShoppingRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ShoppingViewModel(
+        private val repository: ShoppingRepository
+) : ViewModel() {
+
+    fun upsert(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
+        repository.upsert(item)
+    }
+
+    fun delete(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
+        repository.delete(item)
+    }
+
+    fun getAllShoppingItems() = repository.getAllShoppingItems()
+}
